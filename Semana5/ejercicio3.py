03d0e7c9f8ac6cc094489014efd2df5d259a4912
ignacio pavez <paveznacho11@gmail.com>
2026-04-02 14:34:45 +0000

    ejerciico terminado

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -8,9 +8,12 @@ Debe implementar una versión iterativa y una recursiva.
 def factorial_ciclo(n):
     fac = 1
     for i in range (1, n+1):
-        fac = fac*n
+        fac = fac*i
     return fac
 
 
 def factorial_recursivo(n):
-    pass
+    if n == 0:
+        return 1
+    else:
+        return n*(factorial_recursivo(n-1))
