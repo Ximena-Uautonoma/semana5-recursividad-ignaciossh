59a2ac39fb9d453aed8386bcb65c9a890cd69362
ignacio pavez <paveznacho11@gmail.com>
2026-04-01 23:39:21 -0300

    cree ambas funciones

--- Semana5/ejercicio2.py
+++ Semana5/ejercicio2.py
@@ -11,11 +11,18 @@ def suma_ciclo(n):
     """
     Retorna la suma de los primeros n números usando un ciclo.
     """
-    pass
+    suma = 0
+    for i in range(1, n + 1):
+        suma += i
+    return suma
 
 
 def suma_recursiva(n):
     """
     Retorna la suma de los primeros n números usando recursividad.
     """
-    pass
+   if n <= 1:
+        return n
+    
+    else:
+        return n + suma_recursiva(n - 1)
